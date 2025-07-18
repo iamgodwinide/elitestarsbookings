f59a7f85aa3e54443211e82cd8cfd34c2a0cab92
iamgodwinide <iamgodwinide@gmail.com>
2025-07-18 15:13:45 +0100

    new

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ const geistMono = Geist_Mono({
 
 export const metadata: Metadata = {
   title: "Elite Stars Booking- Celebrity Booking Platform",
-  description: "Book your favorite celebrities for events and experiences",
+  description: "Book your favorite celebrities for events and experiences.",
 };
 
 export default function RootLayout({
